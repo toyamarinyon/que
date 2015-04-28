d59127bc76f172e42261be31e743476fa7bba0fd
toyama satoshi <toyamarinyon@gmail.com>
2015-04-28 11:34:30 +0900

    studying rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid" do
+    user = User.new
+    user.name = "toyamarinyon"
+    expect(user).to be_valid
+  end
+
+  describe "name" do
+    it "should be present" do
+      user = User.new
+      user.name = ""
+      expect(user).not_to be_valid
+    end
+    it "should not be too long" do
+      user = User.new
+      user.name = "a"*51
+      expect(user).not_to be_valid
+    end
+  end
+
 end
