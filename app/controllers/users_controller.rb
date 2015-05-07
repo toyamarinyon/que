559014a51b9319d10202d6ad37e0184aad70f1f6
toyama satoshi <toyamarinyon@gmail.com>
2015-05-07 18:08:46 +0900

    Add user/:id resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      render json: @user, status: :created
+      render json: @user, status: 201
     else
-      errors = { :messages => @user.errors, :status => 422 }
-      render json: errors
+      render json: @user.errors, status: 422
     end
   end
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,10 +12,29 @@ RSpec.describe "Users", type: :request do
       params[:password] = 'foobar'
     end
 
-    it "creates post resource" do
+    it "creates user resource" do
       post users_path, user: params
       expect(response).to have_http_status(201)
     end
 
+    it "response error message when param invalid" do
+      params[:name] = ''
+      params[:email] = ''
+      params[:password] = ''
+      post users_path, user: params
+      expect(response).to have_http_status(422)
+    end
+
   end
+
+  describe "GET /users/:id" do
+
+    it "show user resource" do
+      user = create :user
+      get user_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+  end
+
 end
